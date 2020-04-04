f417adc7d27b32ea8d9f00a2cfeedd54d8fced87
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-04-04 11:18:18 +0530

    final commit

--- bin/coindex-key.js
+++ bin/coindex-key.js
@@ -1,18 +1,18 @@
-const { program } = require('commander');
+const program = require('commander');
+const key = require('../commands/key');
 
 program
   .command('set')
-  .description('Set API key -- Get at https://nomics.com')
-  .action(() => console.log('Hello from set'));
+  .description('Set API Key -- Get at https://nomics.com')
+  .action(key.set);
 
-program
-  .command('show')
-  .description('Show API key')
-  .action(() => console.log('Hello from show'));
+program.command('show').description('Show API Key').action(key.show);
 
-program
-  .command('remove')
-  .description('Remove API key')
-  .action(() => console.log('Hello from remove'));
+program.command('remove').description('Remove API Key').action(key.remove);
 
 program.parse(process.argv);
+
+// If no args, output help
+if (!process.argv[2]) {
+  program.outputHelp();
+}
